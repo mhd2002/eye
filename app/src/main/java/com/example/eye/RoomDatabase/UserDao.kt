67c8eea063d1c8e2a6bb6505f24bbc2394dbfd10
mahdi <mahdi_ranjbar2002@outlook.com>
2023-10-21 22:36:07 +0330

    database is working 2

---
+++ app/src/main/java/com/example/eye/RoomDatabase/UserDao.kt
@@ -0,0 +1,27 @@
+package com.example.eye.RoomDatabase
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+
+@Dao
+interface UserDao {
+
+    @Insert
+    fun insert(user: User)
+
+    @Update
+    fun update(user: User)
+
+    @Delete
+    fun deleteUser(user: User)
+
+    @Query("DELETE FROM ${DatabaseName.databasename}")
+     fun deleteAll()
+
+    @Query("SELECT * FROM ${DatabaseName.databasename} order by id desc")
+    fun getAllUser():List<User>?
+
+}
