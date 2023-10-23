7f4f3f69ef0d97c9506b651008a379ec3437023c
mahdi <mahdi_ranjbar2002@outlook.com>
2023-10-23 23:03:21 +0330

    show activity ui is ok

--- app/src/main/java/com/example/eye/ShowUserActivity.kt
+++ app/src/main/java/com/example/eye/ShowUserActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import com.example.eye.Parcelable.UserID
 import com.example.eye.databinding.ActivityAddUserAtivityBinding
 import com.example.eye.databinding.ActivityShowUserBinding
+import java.text.DecimalFormat
 
 class ShowUserActivity : AppCompatActivity() {
     lateinit var binding: ActivityShowUserBinding
@@ -14,13 +15,26 @@ class ShowUserActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityShowUserBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        val user : UserID = intent.getParcelableExtra("ok")!!
-       // val i1 = intent.getIntExtra("o")
 
-        binding.textView.text = user.name + user.codeMeli
+        val user : UserID = intent.getParcelableExtra("ok")!!
+
+        binding.tvMoney.text = separateNumber(user.money.toInt())
 
     }
+    fun separateNumber(number: Int): String {
+        val numberString = number.toString()
+        val length = numberString.length
+        val result = StringBuilder()
 
+        for (i in 0 until length) {
+            result.append(numberString[i])
+            if ((i + 1) % 3 == 0 && i != length - 1) {
+                result.append(",")
+            }
+        }
+
+        return result.toString()
+    }
     override fun onDestroy() {
         finish()
         super.onDestroy()
